9c3ac758a4e7513695fc00833ab338b394ec9697
Syed Abdurrehman Nizami <syedabdurrehman859@gmail.com>
2024-09-29 02:32:45 +0500

    initial commit

---
+++ 4_arithmetic_operators.py
@@ -0,0 +1,34 @@
+#       Arithmetic Operators
+#       Used to perform mathematical operations.
+
+#       + : Addition
+#       - : Subtraction
+#       * : Multiplication
+#       / : Division
+#       % : Modulus (remainder)
+#       ** : Exponentiation (power)
+#       // : Floor division (division that rounds down to the nearest integer)
+
+
+num1 = 20 
+num2 = 5
+
+ans = num1+num2
+print('After adding : ', ans)
+
+ans = num1-num2
+print('After subtracting : ', ans)
+
+ans = num1*num2
+print('After multiplication : ', ans)
+
+ans = num1/num2
+print('After division : ', ans)
+
+ans = num1**num2  # num1^num2  power
+print('After Exponentiation : ', ans)
+
+ans = num1//num2  # also called integer division
+print('After Floor division : ', ans)
+
+
