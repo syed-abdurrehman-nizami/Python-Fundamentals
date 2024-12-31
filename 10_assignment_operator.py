e61aef95924c3103491732ab0209a41e60f1412c
Syed Abdurrehman Nizami <syedabdurrehman859@gmail.com>
2024-12-31 20:57:24 +0500

    initial commit

---
+++ 10_assignment_operator.py
@@ -0,0 +1,48 @@
+#       Assignment Operators
+#       Used to assign values to the variables
+
+#       = : assign
+#       += : Add and assign
+#       -= : Subtract and assign
+#       *= : Multiply and assign
+#       /= : Division and assign
+#       %= : Modulus (remainder) and assign
+#       **= : Exponentiation (power) and assign
+#       //= : Floor division and assign
+
+a = 2
+a += 5 
+print(a)
+
+b = 2
+b -= 4
+print(b)
+
+c = 10
+c *= 2
+print(c)
+
+a = 4
+a /= 2
+print(a)
+
+b = 10
+b /= 4
+print(b)
+
+c = 20
+c %= 3
+print(c)
+
+a = 3
+a **= 3
+print(a)
+
+b = 22.5
+b //= 3
+print(b)
+
+
+
+
+
